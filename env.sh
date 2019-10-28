40a9af8287579872ea663d4685af89e49093a055
bit6tream <bit6stream@gmail.com>
2019-10-28 19:05:47 +0300

    some minor changes

--- env.sh
+++ env.sh
@@ -2,4 +2,4 @@
 
 ! [ -f "i686-elf-4.9.1-Linux-x86_64/" ] && tar xf 'i686-elf-4.9.1-Linux-x86_64.tar.xz'
 
-PS1="(vtos_env) $PS1" PATH="$(pwd)/i686-elf-4.9.1-Linux-x86_64/bin:$PATH" $SHELL
+PROMPT="(vtos_env) $PROFILE" PS1="(vtos_env) $PS1" PATH="$(pwd)/i686-elf-4.9.1-Linux-x86_64/bin:$PATH" $SHELL
