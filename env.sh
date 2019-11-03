087e39c9e6a465cade5fd08911db880ff3a22f71
vt <vt_@gitlab.com>
2019-11-03 14:55:24 +0300

    FINAL: compiler fixes

--- env.sh
+++ env.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-tar -xf ./i686-elf.tar.xz
+tar -xf ./i686-elf.tar.gz
 
 PROMPT="(vtos_env) $(echo $PROMPT)" 
 PS1="(vtos_env) $(echo $PS1)" 
--- vtos-release.h
+++
@@ -1,3 +0,0 @@
-int vtos-release-int = 1;
-char * vtos-release-char = "1";
-char * vtos-release-info = "vtOS 1 | 3 Nov 2019";
