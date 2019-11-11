5a311ca3d49a7aef795d2eb744b862d2bad2e6b3
tvsclass <tvsclass@gmail.com>
2019-11-11 05:53:01 +0000

    +'-n'

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 CC="i686-elf-gcc"
 AS="i686-elf-as"
-CFLAGS="-std=gnu99 -ffreestanding -O0  -c"
+CFLAGS="-std=gnu99 -ffreestanding -O0  -c "
 
 while [[ $1 ]]; do
 	case $1 in
@@ -13,6 +13,14 @@ while [[ $1 ]]; do
 		;;
 		'--hide-1')
 			hide1=1
+		;;
+		'-k' | '--only-kernel')
+			onlykernel=1
+		;;
+		'-n' | '--only')
+			shift 
+			only=1
+			whatonly=$1
 		;;
 	esac
 	shift
@@ -86,6 +94,19 @@ OPT(){
 }
 
 
+if [[ $onlykernel ]]; then
+$(CC) $(CFLAGS) kernel.c                -o ./bin/kernel.o
+$(CC) -T linker.ld -o vtos.bin -ffreestanding -O0 -nostdlib ./bin/*.o  -lgcc
+exit 0;
+fi
+
+if [[ $only ]]; then
+wo=$(ls $whatonly)
+wo1=${wo::-2}
+$(CC) $(CFLAGS) ${whatonly}               -o ./bin/$wo1.o
+$(CC) -T linker.ld -o vtos.bin -ffreestanding -O0 -nostdlib ./bin/*.o  -lgcc
+exit 0;
+fi
 
 ################
 
