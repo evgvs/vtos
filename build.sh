973bbbae9a0a3682efb79fe4b798be8898fa3392
tvsclass <tvsclass@gmail.com>
2019-11-11 04:09:00 +0000

    fix

--- build.sh
+++ build.sh
@@ -49,7 +49,7 @@ make(){
 
 
 CCG(){
-	echo -e "\e[32m$CC $*\e[32m"
+	echo -e "Executing: " "\e[32m$CC $*\e[32m"
 	if [[ $hide1 ]]; then
 		! $CC $* 1>/dev/null && exit 2;
 	else
@@ -58,7 +58,7 @@ CCG(){
 }
 
 ASM(){
-	echo -e "\e[32m$AS $*\e[32m"
+	echo -e "Executing: " "\e[32m$AS $*\e[32m"
 	if [[ $hide1 ]]; then
 		! $AS $* 1> /dev/null && exit 2;
 	else
