615fa42f3fdc116db0a9d995baf70887000adeef
Vladimir <4202342-vt_@users.noreply.gitlab.com>
2019-11-16 15:37:45 +0300

    Panic reboot

--- panic.c
+++ panic.c
@@ -1,6 +1,4 @@
-#include "./drv/video.h"
 #include "panic.h"
-
 void panic(char * text)
 {
 	display_clear(0x4f);
@@ -11,6 +9,7 @@ void panic(char * text)
 	tty_printf("' \n", 0x4f);
 	tty_printf("Before reporting a bug make sure you are using the latest version\n", 0x4f);
 	tty_printf("from our git repository: gitlab.com/vt_/vtos \n \n", 0x4f);
-	tty_printf("System halted. \n", 0x4f);
-	for(;;);
+	tty_printf("Press any key to reboot. \n", 0x4f);
+	keyboard_getchar();
+	reboot();
 }
