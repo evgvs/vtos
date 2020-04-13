996e1ea38bfa21074aabc88454ae9700deba1f5d
Valery Rynochkov <tvsclass@gmail.com>
2020-04-13 15:21:29 +0000

    add hex2dec

--- lib/src/string.c
+++ lib/src/string.c
@@ -244,3 +244,34 @@ void* memmove(void* dst, void* src, size_t n)
 	}
 	return dst;
 }
+
+#include <stdio.h>
+
+long hex2dec(char * a) {
+	char c;
+	long n = 0;
+	
+	while (*a) {
+		c = *a++;
+
+		if (c >= '0' && c <= '9') {
+			c -= '0';
+
+		} else if (c >= 'a' && c <= 'f') {
+			c = (c - 'a') + 10;
+
+		} else if (c >= 'A' && c <= 'F') {
+			c = (c - 'A') + 10;
+
+		} else {
+			goto INVALID;
+		}
+
+		n = (n << 4) + c;
+	}
+
+	return n;
+
+INVALID:
+	return -1;
+}
